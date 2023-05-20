0a2a1a3512ff817595a1e512dcb37b9632b1c872
Randy Yang <ryang.developer@gmail.com>
2023-05-20 17:34:21 -0400

    Added Player Projectiles

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -2,7 +2,11 @@ class_name Enemy
 extends CharacterBody2D
 
 signal sig_death
+signal sig_add_health
+signal sig_health_updated
+signal sig_subtract_health
 
+@export var max_health := 3
 @export var health := 3
 @export var damage := 1
 @export var defense := 0
@@ -13,7 +17,8 @@ signal sig_death
 
 ## Connect signals to functions within script
 func _connect_signals():
-	pass
+	sig_add_health.connect(add_health) 
+	sig_subtract_health.connect(subtract_health)
 
 
 func _ready():
@@ -27,5 +32,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+func add_health(amount: int) -> void:
+	if (health < max_health):
+		if (health + amount > max_health):
+			health = max_health
+		else:
+			health += amount
+		sig_health_updated.emit(health)
+
+
+func subtract_health(amount: int) -> void:
+	health -= amount
+	if health <= 0:
+		die()
+	sig_health_updated.emit(health)
+
+
 func die() -> void:
+	sig_death.emit()
 	queue_free()
