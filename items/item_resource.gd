a6486af518d3cd72384048d969d67002c9d85db1
Randy Yang (ext.) <ryang.developer@gmail.com>
2023-05-21 11:02:12 -0400

    Initial refactor

--- items/item_resource.gd
+++ items/item_resource.gd
@@ -10,6 +10,21 @@ class_name ItemResource
 @export var texture: AtlasTexture
 @export var modifiers: Array[Modifier]
 
-func apply_modifiers(body: PhysicsBody2D) -> void:
-	for modifier in modifiers:
-		modifier.apply(body)
+func apply_modifier(body: PhysicsBody2D) -> void:
+	if (body.data and body.data is ModifierResource):
+		if (modifiers.health):
+			body.sig_add_health.emit(modifiers.health)
+		if (modifiers.speed):
+			body.sig_add_speed.emit(modifiers.speed)
+		if (modifiers.damage):
+			body.sig_add_damage.emit(modifiers.damage)
+		if (modifiers.defense):
+			body.sig_add_defense.emit(modifiers.defense)
+		if (modifiers.magic):
+			body.sig_add_magic.emit(modifiers.magic)
+		if (modifiers.shot_power):
+			body.sig_add_shot_power.emit(modifiers.shot_power)
+		if (modifiers.shot_speed):
+			body.sig_add_shot_speed.emit(modifiers.shot_speed)
+		if (modifiers.score):
+			body.sig_add_score.emit(modifiers.score)
