fce4ff79e2d31c6bed14902bbb769060792d3c67
Shuntoon <83526171+Shuntoon@users.noreply.github.com>
2023-05-23 15:58:21 -0400

    Added more special tiles

--- LevelDesign/level_transition.gd
+++ LevelDesign/level_transition.gd
@@ -5,11 +5,11 @@ extends CanvasLayer
 @onready var marker_2d = $Marker2D
 @onready var myLevelList = preload("res://LevelDesign/LevelList1.tres")
 
-var Main = preload("res://main.tscn")
 var current_level_index = 0
 var real_current_level_index = 1
+@export var level_max = 3
 
-@export var level_index_shuffler = 5
+@export var level_index_shuffler = 3 #how ever many levels it takes before list is shuffled
 
 signal easy_level_beat
 
@@ -23,16 +23,21 @@ func _process(delta):
 	pass
 	
 func next_level_easy():
-	real_current_level_index += 1
-	current_level_index += randi_range(1,2)
-	#Main.add_child(myLevelList.levelList[1])
-	var tween = create_tween()
-	tween.tween_property(color_rect,"position", marker_2d.position, .5)
-	tween.tween_property(color_rect,"position", color_rect.position,.5)
-	timer.start()
-	if real_current_level_index % level_index_shuffler == 0:
-		myLevelList.levelList.shuffle()
-		print(str(myLevelList.levelList[1]))
+	#level switch
+	if real_current_level_index <= level_max:
+		real_current_level_index += 1
+		current_level_index += randi_range(1,2)
+		#Main.add_child(myLevelList.levelList[1])
+		var tween = create_tween()
+		tween.tween_property(color_rect,"position", marker_2d.position, .5)
+		tween.tween_property(color_rect,"position", color_rect.position,.5)
+		timer.start()
+		#randomise after how ever many levels
+		if real_current_level_index % level_index_shuffler == 0:
+			myLevelList.levelList.shuffle()
+			print(str(myLevelList.levelList[1]))
+	elif real_current_level_index > level_max:
+		print("You win!")
 
 func _on_easy_level_beat():
 	next_level_easy()
